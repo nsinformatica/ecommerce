a31a27de8a84466405658081831d52dc1f7689a2
Neto Sales <contato@netosales.com.br>
2018-01-06 13:29:17 -0300

    Arquivos da Aula 100. Apos erros.

--- index.php
+++ index.php
@@ -2,19 +2,26 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
+	$page = new Page();
+	$page->setTpl("index");
+	
 	//echo "OK";
-	$sql = new Hcode\DB\Sql();
-	$slq->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	//$sql = new Hcode\DB\Sql();
+	//$results = $sql->select("SELECT * FROM tb_users");
+	//echo json_encode($results);
 
 });
 
 $app->run();
 
- ?>
+?>
