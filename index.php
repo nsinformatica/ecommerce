9ed2cb96cd6825b1d6ffb9c60352cc1b78790b49
Neto Sales <contato@netosales.com.br>
2018-01-11 14:11:06 -0300

    Finalizacao aula 104. Funcional.

--- index.php
+++ index.php
@@ -81,6 +81,11 @@ $app->get("/admin/users/create", function(){
 });
 $app->get("/admin/users/:iduser/delete", function($iduser){
 	User::verifyLogin();
+	$user = new User();
+	$user->get((int)$iduser);
+	$user->delete();
+	header("Location: /admin/users");
+	exit;
 	
 
 });
@@ -89,16 +94,30 @@ $app->get("/admin/users/:iduser/delete", function($iduser){
 
 
 $app->get("/admin/users/:iduser", function($iduser){
+	
 	User::verifyLogin();
+	
+	$user = new User();
+	$user->get((int)$iduser);
+
 	$page = new PageAdmin();
-	$page->setTpl("users-update");
+	
+	$page->setTpl("users-update", array(
+
+			"user"=>$user->getValues()
+		));
 
 });
 $app->post("/admin/users/create", function(){
 	User::verifyLogin();
 	
 	$user = new User();
+	
+	$_POST["inadmin"] = (isset($_POST))?1:0;
+
 	$user->setData($_POST);
+		
+
 	$user->save();
 
 	//var_dump($user);
@@ -108,7 +127,13 @@ $app->post("/admin/users/create", function(){
 });
 $app->post("/admin/users/:iduser", function($iduser){
 	User::verifyLogin();
-	
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST))?1:0;
+	$user->get((int)$iduser);
+	$user->setData($_POST);
+	$user->update();
+	header("Location: /admin/users");
+	exit;
 
 });
 
