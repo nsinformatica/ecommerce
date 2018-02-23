5687b78774d7211572d5f59e04dfe23cd6d50e02
Neto Sales <contato@netosales.com.br>
2018-02-22 22:05:18 -0300

    Aula 111. Erro na exibicao dos produtos.

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -79,6 +79,33 @@ class Category extends Model{
 				]);
 		}
 	}
+	public function getProductsPage($page = 1, $itemsPerPage = 2)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+		$sql = new Sql();
+		$results = $sql->select("
+				SELECT SQL_CALC_FOUND_ROWS *
+				FROM tb_products a
+				INNER JOIN tb_productscatgories b ON a.idproduct = b.idproduct
+				INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+				WHERE c.idcategory = :idcategory
+				LIMIT $start, $itemsPerPage;
+
+				", [
+					':idcategory'=>$this->getidcategory()
+
+				]);
+				$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+				return [
+					'data'=>Products::checkList($results),
+					'total'=>(int)$resultTotal[0]["nrtotal"],
+					'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+				];
+	}
+
+
 	public function addProduct(Products $product)
 	{
 		$sql = new Sql();
