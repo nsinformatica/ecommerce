b0815ab41020e9ed164e8f323ce254217da0f467
Neto Sales <contato@netosales.com.br>
2018-02-28 11:27:11 -0300

    Aula 111. Erro corrigido. exibicao produtos android.

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -87,7 +87,7 @@ class Category extends Model{
 		$results = $sql->select("
 				SELECT SQL_CALC_FOUND_ROWS *
 				FROM tb_products a
-				INNER JOIN tb_productscatgories b ON a.idproduct = b.idproduct
+				INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
 				INNER JOIN tb_categories c ON c.idcategory = b.idcategory
 				WHERE c.idcategory = :idcategory
 				LIMIT $start, $itemsPerPage;
