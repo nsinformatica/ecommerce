fc12c23b3f46387b8d9dd0b974292da6f27cb744
Neto Sales <contato@netosales.com.br>
2018-03-02 13:22:40 -0300

    Aula 112. Sem nenhum erro.

--- site.php
+++ site.php
@@ -40,5 +40,14 @@ $app->get("/categories/:idcategory", function($idcategory){
 			'pages'=>$pages
 		]);
 });
+$app->get("/products/:desurl", function($desurl){
+	$product = new Products();
+	$product->getFromURL($desurl);
+	$page = new Page();
+	$page->setTpl("product-detail", [
+			'product'=>$product->getValues(),
+			'categories'=>$product->getCategories()
+		]);
+});
 
 ?> 
